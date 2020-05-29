1c5dec9df36c6061ef9c90a600850c6e9d21846d
Nikola Marunic <nikimarunic@gmail.com>
2020-05-29 10:09:30 -0400

    Fixed bug where removing a stock would always graphically stop displaying the most recently added one, but would remove the correct one from the holdings list

--- src/Components/Stock/Stock.js
+++ src/Components/Stock/Stock.js
@@ -34,26 +34,22 @@ class Stock extends React.Component {
 
   handleNameChange(event) {
     let newStock = {name: this.props.stock.name, allocation: this.props.stock.allocation, newName: event.target.value};
-    console.log(newStock);
     this.setState({name: newStock.newName, alloc: this.props.stock.allocation});
-
     this.props.handleNameChange(newStock);
   }
 
   handleAllocChange(event){
     let newStock = {name: this.props.stock.name, allocation: this.props.stock.allocation, newAlloc: Number(event.target.value)};
     this.setState({alloc: newStock.newAlloc, name: this.props.stock.name});
-
     this.props.handleAllocChange(newStock);
-
   }
 
   render() {
     return (
       <div className="Stock">
         <div className="Stock-information">
-          <input key={this.props.stock.key}  type= 'text' value = {this.state.name} className="stockName" onChange = {this.handleNameChange}></input>
-          <input key={this.props.stock.key} type='number' value = {this.state.alloc} className="allocation" onChange = {this.handleAllocChange}></input>
+          <input key={this.props.stock.key}  type= 'text' value = {this.props.stock.name} className="stockName" onChange = {this.handleNameChange}></input>
+          <input key={this.props.stock.key} type='number' value = {this.props.stock.allocation} className="allocation" onChange = {this.handleAllocChange}></input>
         </div>
         {this.renderAction()}
       </div>
